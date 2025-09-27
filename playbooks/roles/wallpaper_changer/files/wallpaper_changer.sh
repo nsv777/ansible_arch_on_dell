b0100f45323e9c7d7f272bb6d8be0437250a6c12
Stanislav Nilov <nsv777@gmail.com>
2025-09-27 13:31:14 +0200

    updated wallpaper_changer

--- playbooks/roles/wallpaper_changer/files/wallpaper_changer.sh
+++ playbooks/roles/wallpaper_changer/files/wallpaper_changer.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 tempfile=$(mktemp)
-wallpaper_file="/var/tmp/background.jpg"
+wallpaper_file_prefix="/var/tmp/background"
 
 PYTHON_BINARY="${1:?Error: Python binary path is required as the first argument.}"
 SCRIPT_PATH="${2:?Error: Script path is required as the second argument.}"
@@ -17,8 +17,16 @@ GSETTINGS_WALLPAPER_PARAMETER="${3:?Error: GSettings wallpaper parameter is requ
 WALLPAPER_URL=$("${PYTHON_BINARY}" "${SCRIPT_PATH}")
 
 wget -q --limit-rate=50k -O "${tempfile}" "${WALLPAPER_URL}"
+
+# Checking the file type. It is not always jpeg
+# taking the second part of image/png or image/jpeg
+file_type=$(file --brief --mime-type "${tempfile}" | cut -d '/' -f2)
+# Rename temp file accordingly
+wallpaper_file="${wallpaper_file_prefix}.${file_type}"
 mv "${tempfile}" "${wallpaper_file}"
 
-PID=$(pgrep -f 'gnome-session' | head -n1)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)
+#PID=$(pgrep -f 'gnome-session' | head -n1)
+#export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)
+
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 gsettings set "$GSETTINGS_WALLPAPER_PARAMETER" picture-uri "file://${wallpaper_file}"
